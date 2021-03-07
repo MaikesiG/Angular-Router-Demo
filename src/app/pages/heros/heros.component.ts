5bab774312c288a234dabbc8a9202c343b8c6fc0
J1NTA <snow93star@gmail.com>
2021-03-07 00:55:13 -0500

    init and finished

--- src/app/pages/heros/heros.component.ts
+++ src/app/pages/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero, HeroArg } from 'src/app/configs/type';
+import { HeroService } from 'src/app/services/hero.service';
 import Heros from '../../configs/hero'
 @Component({
   selector: 'app-heros',
@@ -13,7 +14,12 @@ export class HerosComponent implements OnInit {
     sort: 'desc'
   };
   heros: Hero[] = Heros;
-  constructor() { }
+  // heroServe:HeroService;
+  constructor(readonly heroServe:HeroService) {
+    // this.heroServe = new HeroService()
+    this.heros = this.heroServe.getHeros();
+    console.log('heroServe', this.heroServe.getHeros())
+   }
 
   ngOnInit(): void {
   }
