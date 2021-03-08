b704f12c7deebbc5ac4ea8bdeae307463f50d175
J1NTA <snow93star@gmail.com>
2021-03-08 00:06:42 -0500

    几种写法，snapshot

--- src/app/routerS/heroes/hero-detail/hero-detail.component.ts
+++ src/app/routerS/heroes/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -10,17 +13,19 @@ import { HeroService } from '../hero.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroDetailComponent implements OnInit {
-
-  constructor(private route:ActivatedRoute, private heroServe:HeroService) { }
+  hero$:Observable<Hero>;
+  constructor(private route:ActivatedRoute, private heroServe:HeroService,
+    private router:Router,) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params=>{
-      // console.log('params', params.get('id'))
-      this.heroServe.getHero(params.get('id')).subscribe( hero => {
-        console.log('hero', hero)
+    // console.log('this.route.paramMap', this.route.paramMap)
+    this.hero$=this.route.paramMap.pipe(
+      switchMap( params=> {
+        return this.heroServe.getHero(params.get('id'));
       })
-
-    })
+    )
   }
-
+  back(id:number) {
+    this.router.navigate(['/heroes',{id}]); //key value一样，es6可以直接写
+  }
 }
--- src/app/routerS/heroes/hero-list/hero-list.component.ts
+++ src/app/routerS/heroes/hero-list/hero-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -12,19 +13,36 @@ import { HeroService } from '../hero.service';
 export class HeroListComponent implements OnInit {
   heroes$: Observable<Hero[]>;
   selectedId: number;
-  constructor(private heroServe:HeroService, private router:Router) {
+  constructor(
+    private heroServe:HeroService,
+    private router:Router,
+    private route:ActivatedRoute,
+
+    ) {
     // console.log('this.heroServe.getHeroes()', this.heroServe.getHeroes())
   }
 
+  // ngOnInit(): void {
+  //   this.heroes$ =  this.route.paramMap.pipe(
+  //     switchMap( params => {
+  //     this.selectedId = +params.get('id');
+  //     // console.log('params', params);
+  //     return this.heroServe.getHeroes();
+  //   }))
+  //   // this.heroServe.getHeroes();
+  //   // console.log('this.heroes$', this.heroes$)
+  // }
+
+  //snapshot写法
   ngOnInit(): void {
-    this.heroes$ =  this.heroServe.getHeroes();
-    console.log('this.heroes$', this.heroes$)
+    this.selectedId = +this.route.snapshot.paramMap.get('id');
+    this.heroes$ = this.heroServe.getHeroes();
   }
   onSelected(id:number) {
     this.selectedId = id;
     // this.router.navigateByUrl('/hero/' + id);
-    this.router.navigate(['/hero/' + id]);
-    this.router.navigate(['/hero',id]);
+    // this.router.navigate(['/hero/' + id]);
+    this.router.navigate(['/hero',id]); //不加斜杠也行，后面可以无限接参数,自动补全斜杠
   }
 
 }
